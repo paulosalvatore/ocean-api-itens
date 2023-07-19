5e188748cc4bd534c3a38859eb3896a3349d1fe5
Paulo Salvatore <salvatoregames@gmail.com>
2023-07-19 11:57:33 -0300

    Cors habilitado

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+// Middlewares
+
+app.use(cors());
 app.use(express.json());
+
+// Dados
 
 const item1 = {
   nome: "Rick Sanchez",
